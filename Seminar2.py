765274216a1bbb568a45844d067c4bdd45beff38
Valentin <burov0742@gmail.com>
2022-09-06 13:38:10 +0300

    Seminar 2 added

---
+++ Seminar2.py
@@ -0,0 +1,14 @@
+# закономерность для известного количества чисел последовательности
+
+# number_N = int (input('введите число N: '))
+# for i in range (number_N):
+#     print ((-3)**i, end=' ')
+
+#сравнение двух строк. Есть ли в первой строке полное совпадение второй строки
+
+# string_1 = input()
+# string_2 = input()
+# if string_1 > string_2:
+#     print (string_1.count(string_2)) #свтроенная функция count для поиска строк 
+# else:
+#     print (string_2.count(string_1))
